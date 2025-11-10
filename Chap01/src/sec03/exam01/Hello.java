ac60a6680ff49f01b55decc22c427acd621ec65b
JoonHee Choi <gameboy960501@gmail.com>
2025-11-10 16:32:43 +0900

    dd

---
+++ Chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,30 @@
+package sec03.exam01;
+
+// 1.각자 테마 변경 및 글자 크기 조정
+// 단축키 : ctrl +  또는 -
+
+// 2.주석 사용하기
+// 여기에는 모두 주석이 됩니다.
+/*
+ 주석1
+ 주석2
+ */
+/**
+ * 도큐먼트 주석
+ */
+// TODO : tasks 뷰 추가해서 확인
+
+
+public class Hello { //클래스 선언 : 클래스 이름은 항상 대문자로 시작하고 반드시 소스 파일 이름과 동일해야함.
+	
+	// 프로그램 실행 진입점
+	// main 메소드 : 자바 프로그램 실행 시 가장 먼저 실행되는 메소드
+
+	public static void main(String[] args) { // 메소드 선언
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+		
+		System.out.println("Hello, // /**/ Java");
+	}
+
+}
